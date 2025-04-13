faed0a4afe8e68e8e5e31e5d109427b7a265bca0
Kobmanshik <kostya01wonder@gmail.com>
2025-04-13 23:28:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import API_TOKEN
 from handlers import topup, supplies
 
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=7829191204:AAF3utRWorh8gVGp-JbLngiZlaog4F6gf7k)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 # Регистрация хендлеров
