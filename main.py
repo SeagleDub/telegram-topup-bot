5bc1938ec45d0876dac19d69327fc37d5ccd30f9
Nikita <grebenikit@gmail.com>
2025-04-29 22:00:11 +0300

    fix

--- main.py
+++ main.py
@@ -183,9 +183,9 @@ async def upload_images(message: Message, state: FSMContext):
     )
     
     gc = gspread.authorize(None)
-    spreadsheet = gc.open_by_key(GOOGLE_SHEET_ID)
-    worksheet = spreadsheet.sheet1
-    worksheet.append_row([order_id, username, user_id, offer_name, category, specification])
+    gs = gc.open_by_key(GOOGLE_SHEET_ID)
+    worksheet1 = gs.worksheet('Sheet1')
+    worksheet1.append_row([order_id, username, user_id, offer_name, category, specification])
     
     await message.answer(f"Ваша заявка {order_id} отправлена администратору.", reply_markup=menu_kb)
     await state.clear()
