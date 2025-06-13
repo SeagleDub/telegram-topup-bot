257085d6a515d0a4bdf18732429455ea2e1015dd
Nikita Grebenyuk <ngre@efisco.net>
2025-06-14 00:52:20 +0300

    test

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ async def send_broadcast(query: CallbackQuery, state: FSMContext):
     await query.answer("Начинаю рассылку...")
     data = await state.get_data()
     messages = data.get("broadcast_messages", [])
-    user_ids = await get_user_ids_from_sheet()
+    user_ids = get_user_ids_from_sheet()
 
     if not user_ids:
         await query.message.answer("⚠️ Список пользователей пуст. Рассылка отменена.", reply_markup=menu_kb_admin)
@@ -172,9 +172,9 @@ async def cancel_broadcast(query: CallbackQuery, state: FSMContext):
 
 @router.message(F.text == "🌐 Создать/починить лендинг")
 async def create_landing(message: Message, state: FSMContext):
-    # if not is_user_allowed(message.from_user.id):
-    #     await message.answer("❌ У вас нет доступа к этой функции.")
-    #     return
+    if not is_user_allowed(message.from_user.id):
+        await message.answer("❌ У вас нет доступа к этой функции.")
+        return
     kb = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="💻 Создать лендинг", callback_data="landing:create")],
         [InlineKeyboardButton(text="🔧 Починить лендинг", callback_data="landing:repair")]
@@ -219,9 +219,9 @@ ready_kb = ReplyKeyboardMarkup(
 
 @router.message(F.text == "🖼️ Уникализатор")
 async def images_unicalization_initiation(message: Message, state: FSMContext):
-    # if not is_user_allowed(message.from_user.id):
-    #     await message.answer("❌ У вас нет доступа к этой функции.")
-    #     return
+    if not is_user_allowed(message.from_user.id):
+        await message.answer("❌ У вас нет доступа к этой функции.")
+        return
     m1 = await message.answer("Загрузите изображение для уникализации (одно изображение)")
     m2 = await message.answer("❌ В любой момент нажмите 'Отмена', чтобы выйти", reply_markup=cancel_kb)
     last_messages[message.from_user.id] = [m1.message_id, m2.message_id]
@@ -561,9 +561,9 @@ async def upload_zip_file(message: Message, state: FSMContext):
 
 @router.message(F.text == "💰 Заказать пополнение")
 async def order_topup(message: Message, state: FSMContext):
-    # if not is_user_allowed(message.from_user.id):
-    #     await message.answer("❌ У вас нет доступа к этой функции.")
-    #     return
+    if not is_user_allowed(message.from_user.id):
+        await message.answer("❌ У вас нет доступа к этой функции.")
+        return
     kb = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="🏦 AdsCard", callback_data="bank:adscard"),
          InlineKeyboardButton(text="💳 Traffic.cards", callback_data="bank:trafficcards")]
@@ -637,14 +637,9 @@ async def type_selected(query: CallbackQuery, state: FSMContext):
 
 @router.message(F.text == "📂 Запросить расходники")
 async def request_supplies(message: Message, state: FSMContext):
-    gc = gspread.service_account(filename='credentials.json')
-    table = gc.open_by_key(GOOGLE_SHEET_ID)
-    worksheet = table.get_worksheet(1)
-    user_ids = worksheet.col_values(1)
-    await message.answer(str(user_ids))
-    # if not is_user_allowed(message.from_user.id):
-    #     await message.answer("❌ У вас нет доступа к этой функции.")
-    #     return
+    if not is_user_allowed(message.from_user.id):
+        await message.answer("❌ У вас нет доступа к этой функции.")
+        return
     kb = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="👤 Аккаунты", callback_data="supply:accounts")],
         [InlineKeyboardButton(text="🌐 Домены", callback_data="supply:domains")]
@@ -829,10 +824,10 @@ def is_user_allowed(user_id: int) -> bool:
 
     return user_id in user_ids
 
-async def get_user_ids_from_sheet() -> list[int]:
+def get_user_ids_from_sheet() -> list[int]:
     gc = gspread.service_account(filename='credentials.json')
     table = gc.open_by_key(GOOGLE_SHEET_ID)
-    worksheet = table.sheet2
+    worksheet = table.get_worksheet(1)
     user_ids = worksheet.col_values(1)
 
     return [int(user_id) for user_id in user_ids if user_id.isdigit()]
